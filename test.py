4b95933e90a24266d235f76d3b9313649379415b
Angelacod <47693807+Angelacod@users.noreply.github.com>
2019-05-26 17:02:44 +0100

    adding two numbers

--- test.py
+++ test.py
@@ -7,4 +7,10 @@ like = swe
     print "{} plus {}.".format(number1,number2)
 add_two_numbers(5,2)
 
+def add_two_numbers():
+    number1=1
+    number2=2
+    answer=number1+number2
+    print answer
+add_two_numbers()
 
